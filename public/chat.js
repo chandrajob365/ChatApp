f30144abfbc917c511b1aa037b3642957d74e45b
Manish <chandrajob365@gmail.com>
2017-05-23 17:05:20 +0530

    Add events for active users display list

--- public/chat.js
+++ public/chat.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
   var messages = []
   const socket = io.connect()
+  const activeUsers = document.getElementById('activeUsers')
   const field = document.getElementById('data')
   const sendButton = document.getElementById('send')
   const content = document.getElementById('content')
+  var childInnerHTMLArr = []
+  socket.emit('activeUsers')
   sendButton.onclick = function () {
     messages.push(field.value)
     socket.emit('chatMessage', field.value)
@@ -15,4 +18,23 @@ document.addEventListener('DOMContentLoaded', function () {
     li.appendChild(document.createTextNode(text))
     content.appendChild(li)
   })
+  socket.on('activeUsers', (listOfActiveUsers) => {
+    console.log('listOfActiveUsers->', listOfActiveUsers)
+    for (let i = 0; i < listOfActiveUsers.length; i++) {
+      if (nodeExist(listOfActiveUsers[i]) === -1) {
+        let li = document.createElement('li')
+        li.appendChild(document.createTextNode(listOfActiveUsers[i]))
+        activeUsers.appendChild(li)
+      }
+    }
+  })
+  socket.on('updateActiveUsersList', (userName) => {
+    var index = childInnerHTMLArr.indexOf(userName)
+    activeUsers.removeChild(activeUsers.children[index])
+  })
+  function nodeExist (user) {
+    let index = childInnerHTMLArr.indexOf(user)
+    if (index === -1) childInnerHTMLArr.push(user)
+    return index
+  }
 })

32962cc23f3d7fff5a96ef85b0009d4b7e54b12a
Manish <chandrajob365@gmail.com>
2017-06-02 04:23:49 +0530

    Create route for new phone number with DB entry and redirect to chat_ui

---
+++ routes/newUser.js
@@ -0,0 +1,27 @@
+const UserDB = require('../db/userdb').Users
+
+exports.save = (req, res, next) => {
+  var data = req.body.user
+  //req.session.user = user
+  UserDB.insert(data, (err, user) => {
+    console.log('<newUser.js, save > callback from DB after insert user -> ', user)
+    if (err) throw new Error(err)
+    retriveUserDetailAndRedirect(data.phoneNumber, req, res)
+  })
+  console.log('<newUser.js save > data -> ', data)
+}
+
+const retriveUserDetailAndRedirect = (phoneNumber, req, res) => {
+  console.log('<newUser.js, retriveUserDetailAndRedirect > Entry')
+  UserDB.findByPhoneNumber(phoneNumber, (err, user) => {
+    console.log('<newUser.js, retriveUserDetailAndRedirect > callback from findByPhoneNumber user -> ', user)
+    if(err) throw new Error(err)
+    if(user) {
+      req.session.user = user
+      res.redirect('chatRelay')
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'})
+      res.end('Something went wrong while retriving user detail :( ')
+    }
+  })
+}

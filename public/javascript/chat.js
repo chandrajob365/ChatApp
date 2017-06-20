9ed86a2fc349442c6cb40ae38c2830e91f36f999
Manish <chandrajob365@gmail.com>
2017-06-20 15:32:20 +0530

    Add redis support for messages and group storage,dynamic createRoom, switchRoom methods

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,12 +1,12 @@
 const socket = io.connect()
+var currentGroup = '' // !! update currentRoom as when user switches to diff room !!
 document.addEventListener('DOMContentLoaded', function () {
   var messages = []
-
   const field = document.getElementById('data')
   const sendButton = document.getElementById('send')
   var roomUsersObj = {}
   var rooms = []
-  var currentGroup = '' // !! update currentRoom as when user switches to diff room !!
+
   // socket.emit('activeUsers')
   socket.on('setDefaultRoom', (groupName) => {
     console.log('<client chat.js setDefaultRoom> groupName = ', groupName)
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
     field.disabled = false
     sendButton.disabled = false
   })
-   sendButton.onclick = function () {
+  sendButton.onclick = function () {
      console.log('<client chat.js onclick> currentGroup = ', currentGroup)
      if (currentGroup.length > 0) {
        socket.emit('chatMessage', {
@@ -29,6 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
          text: field.value
        })
      }
+     field.value = ''
     return false
   }
   socket.on('chatMessage', (msg) => {
@@ -40,186 +41,18 @@ document.addEventListener('DOMContentLoaded', function () {
       populateMessagePannel(msg)
     }
   })
+
   socket.on('createGroup', (obj) => {
     field.disabled = false
     sendButton.disabled = false
+    console.log('<chat.js>Inside createGroup -> ', obj)
     populateGroup(obj.groupName)
-    // !! Add method to relay welcome message to all the member of group !!
+    if (currentGroup === '') {
+      currentGroup = obj.groupName
+      populateCurrentGroupName(obj.groupName)
+    }
+    // !! < Method to display system messages > Add method to relay welcome message to all the member of group !!
   })
-  /* joinRoom = function (roomName) {
-    currentRoom = roomName
-    socket.emit('joinRoom', {
-      room: currentRoom
-    })
-    return false
-  } */
-  /* socket.on('chatMessage', (msg) => {
-    createChildNode(getContentDisplayPannelForRoom(msg.room), msg)
-    getIndividualUser(msg.room, getUserListDisplayPannelForRoom(msg.room), msg.users)
-    if (rooms.indexOf(msg.room) === -1) {
-      rooms.push(msg.room)
-      createJoinedRoomNode(alreadyJoined, msg.room)
-    }
-  }) */
-  /* socket.on('joinRoom', (msg) => {
-    currentRoom = msg.room
-    //toggleUsersAndMessageViewPannel(msg.room)
-  }) */
-  /* const toggleUsersAndMessageViewPannel = room => {
-    switch (room) {
-      case 'Default Room':
-        defaultRoomContent.style.display = 'block'
-        room1Content.style.display = 'none'
-        room2Content.style.display = 'none'
-        defaultActiveUsers.style.display = 'block'
-        room1ActiveUsers.style.display = 'none'
-        room2ActiveUsers.style.display = 'none'
-        break
-      case 'Room1':
-        defaultRoomContent.style.display = 'none'
-        room1Content.style.display = 'block'
-        room2Content.style.display = 'none'
-        defaultActiveUsers.style.display = 'none'
-        room1ActiveUsers.style.display = 'block'
-        room2ActiveUsers.style.display = 'none'
-        break
-      case 'Room2':
-        defaultRoomContent.style.display = 'none'
-        room1Content.style.display = 'none'
-        room2Content.style.display = 'block'
-        defaultActiveUsers.style.display = 'none'
-        room1ActiveUsers.style.display = 'none'
-        room2ActiveUsers.style.display = 'block'
-        break
-    }
-  } */
-  //toggleUsersAndMessageViewPannel('Default Room')
-
-  /* const getIndividualUser = (room, userPannel, users) => {
-    for (var user in users) {
-      createRoomUsersObj(room, userPannel, users[user])
-    }
-  } */
-  /* const createRoomUsersObj = (room, userPannel, user) => {
-    if (!roomUsersObj[room]) {
-      roomUsersObj[room] = []
-    }
-    if (roomUsersObj[room].indexOf(user) === -1) {
-      roomUsersObj[room].push(user)
-      createUserListChildNode(userPannel, user)
-    }
-  } */
-  /* const createUserListChildNode = (userPannel, user) => {
-    let p = document.createElement('p')
-    p.appendChild(document.createTextNode(user))
-    p.classList.add('members-msgCount')
-    userPannel.appendChild(p)
-  } */
-
-  /* const createChildNode = (node, msg) => {
-    let article = document.createElement('article')
-    article.classList.add('media')
-    let div1ChildOfArticle = document.createElement('div')
-    div1ChildOfArticle.classList.add('media-left')
-    let figure = document.createElement('figure')
-    figure.classList.add('image', 'is-64x64')
-    let img = document.createElement('img')
-    img.src = 'http://placehold.it/128x128'
-    img.alt = 'Image'
-    figure.appendChild(img)
-    div1ChildOfArticle.appendChild(figure)
-    article.appendChild(div1ChildOfArticle)
-    let div2ChildOfArticle = document.createElement('div')
-    div2ChildOfArticle.classList.add('media-content')
-    let div3ChildOfDiv2 = document.createElement('div')
-    div3ChildOfDiv2.classList.add('content')
-    let p = document.createElement('p')
-    let strong = document.createElement('strong')
-    strong.appendChild(document.createTextNode(msg.senderName))
-    p.appendChild(strong)
-    let br = document.createElement('br')
-    let msgText = document.createTextNode(msg.text)
-
-    p.insertBefore(br, p.appendChild(msgText))
-    div3ChildOfDiv2.appendChild(p)
-    let nav = document.createElement('nav')
-    nav.classList.add('level')
-    let divChildOfNav = document.createElement('div')
-    divChildOfNav.classList.add('level-left')
-    let a = document.createElement('a')
-    a.classList.add('level-item')
-    let span = document.createElement('span')
-    span.classList.add('icon', 'is-small')
-    let i = document.createElement('i')
-    i.classList.add('fa', 'fa-reply')
-    span.appendChild(i)
-    a.appendChild(span)
-    divChildOfNav.appendChild(a)
-    nav.appendChild(divChildOfNav)
-    div2ChildOfArticle.appendChild(div3ChildOfDiv2)
-    div2ChildOfArticle.appendChild(nav)
-    article.appendChild(div1ChildOfArticle)
-    article.appendChild(div2ChildOfArticle)
-    node.appendChild(article)
-  } */
-
-  /* const createJoinedRoomNode = (alreadyJoined, roomName) => {
-    let div1 = document.createElement('div')
-    div1.classList.add('columns')
-
-    let div2ChildOf1 = document.createElement('div')
-    div2ChildOf1.classList.add('column', 'is-3', 'is-marginless')
-    let div3ChildOfDiv2 = document.createElement('div')
-    div3ChildOfDiv2.classList.add('image')
-    let img = document.createElement('img')
-    img.src = 'http://bulma.io/images/placeholders/96x96.png'
-    div3ChildOfDiv2.appendChild(img)
-    div2ChildOf1.appendChild(div3ChildOfDiv2)
-
-    let div4ChildOf1 = document.createElement('div')
-    div4ChildOf1.classList.add('column', 'is-6')
-    let p = document.createElement('p')
-    let strong = document.createElement('strong')
-    strong.appendChild(document.createTextNode(roomName))
-    p.appendChild(strong)
-    div4ChildOf1.appendChild(p)
-
-    let div5ChildOf1 = document.createElement('div')
-    div5ChildOf1.classList.add('column', 'is-3')
-    let button = document.createElement('button')
-    button.classList.add('button', 'is-active', 'is-info')
-    button.value = roomName
-    button.addEventListener('click', function () {
-      console.log('!!!!!!!!!!!Hi......!!!!!!!!roomName= ', roomName)
-      currentRoom = roomName
-      toggleUsersAndMessageViewPannel(currentRoom)
-    }, false)
-    let span = document.createElement('span')
-    span.classList.add('icon', 'is-small')
-    let i = document.createElement('i')
-    i.classList.add('fa', 'fa-toggle-on')
-    span.appendChild(i)
-    button.appendChild(span)
-    div5ChildOf1.appendChild(button)
-
-    div1.appendChild(div2ChildOf1)
-    div1.appendChild(div4ChildOf1)
-    div1.appendChild(div5ChildOf1)
-
-    alreadyJoined.appendChild(div1)
-  } */
-
-  /* const getContentDisplayPannelForRoom = (room) => {
-    if (room === 'Default Room') return defaultRoomContent
-    if (room === 'Room1') return room1Content
-    if (room === 'Room2') return room2Content
-  } */
-  /* const getUserListDisplayPannelForRoom = (room) => {
-    if (room === 'Default Room') return defaultActiveUsers
-    if (room === 'Room1') return room1ActiveUsers
-    if (room === 'Room2') return room2ActiveUsers
-  }
-}) */
 })
 /* Emits event to server to save new group , created by clicking "createGroup" button on UI */
 const createGroup = () => {
@@ -261,6 +94,10 @@ const createGroup = () => {
     div4ChildOfDiv1.classList.add('column', 'is-6')
     let span = document.createElement('span')
     span.style.cursor = 'pointer' // !! Add onclick on span to populate group content !!
+    span.addEventListener('click', function () {
+      console.log('groupName -> ', groupName)
+      switchGroup(groupName)
+    }, true)
     let strong = document.createElement('strong')
     strong.appendChild(document.createTextNode(groupName))
     span.appendChild(strong)
@@ -272,10 +109,26 @@ const createGroup = () => {
     groupUserWrapper.appendChild(div1)
     groupUserWrapper.appendChild(hr)
   }
-const showModal = elementId => {
-  let modal = document.getElementById('modal_newRoom')
-  modal.classList.add('is-active')
-}
+  const switchGroup = groupName => {
+    console.log('Inside switchGroup <chat.js> client side groupName -> ', groupName)
+    currentGroup = groupName
+    console.log('Inside switchGroup <chat.js> client side currentGroup -> ', currentGroup)
+      clearMessagePannel()
+    socket.emit('switchGroup', {
+      name: groupName
+    })
+  }
+  socket.on('populateGroupWithEmptyMessageList', groupName => {
+    console.log('Inside of populateGroupName <chat.js client side> currentGroup = ', currentGroup, 'groupName.name = ', groupName.name)
+    if(currentGroup === groupName.name) {
+      console.log('<chat.js chatMessage> currentGroup === groupName.name')
+      populateCurrentGroupName(groupName.name)
+    }
+  })
+  const showModal = elementId => {
+    let modal = document.getElementById('modal_newRoom')
+    modal.classList.add('is-active')
+  }
 
 const closeModal = elementId => {
   let modal = document.getElementById('modal_newRoom')
@@ -291,7 +144,13 @@ const addToTextBox = obj => {
 const populateCurrentGroupName = groupName => {
   let currentGroup = document.getElementById('currentGroup')
   currentGroup.innerHTML = groupName
+  // currentGroup.addEventListener('click', () => {
+  //   displayGroupMembersCard(groupName)
+  // }, true)
 }
+// const displayGroupMembersCard = groupName => {
+//
+// }
 const populateMessagePannel = msg => {
   let messageContentWrapper = document.getElementById('messageContent')
   let article = document.createElement('article')
@@ -340,4 +199,9 @@ const populateMessagePannel = msg => {
   let hr = document.createElement('hr')
   messageContentWrapper.appendChild(article)
   messageContentWrapper.appendChild(hr)
+}
+
+const clearMessagePannel = () => {
+  let messageContentWrapper = document.getElementById('messageContent')
+  messageContentWrapper.innerHTML = ''
 }

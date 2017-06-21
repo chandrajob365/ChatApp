bc08b76f59117ecb1b6b6d88ee121ad886af1fe6
Manish <chandrajob365@gmail.com>
2017-06-21 11:28:57 +0530

    Modify group-users, user-groups and group-messages storage in rediss

--- db/roomdb.js
+++ db/roomdb.js
@@ -1,6 +1,8 @@
 const redis = require('redis')
 const redisClient = redis.createClient()
-
+const _groupExtn = '_grp'
+const _userExtn = '_users'
+const _messages = '_msgs'
 redisClient.on('connect', function () {
   console.log('connected')
 })
@@ -8,30 +10,45 @@ redisClient.on('error', function (err) {
   throw new Error(err)
 })
 class Rooms {
-  static saveGroupDetailList (obj, cb) {
-    redisClient.hmset('groupDetailObj', {
-      'groupName': obj.groupName,
-      'admin': obj.admin,
-      'users': JSON.stringify(obj.users)
-    })
+  // static saveGroupDetailList (obj, cb) {
+  //   redisClient.hmset('groupDetailObj', {
+  //     'groupName': obj.groupName,
+  //     'admin': obj.admin,
+  //     'users': JSON.stringify(obj.users)
+  //   })
+  //   Rooms.saveUserDetailList(obj)
+  //   redisClient.hgetall('groupDetailObj', (err, groupDetailObj) => {
+  //     if (err) throw new Error(err)
+  //     redisClient.rpush('groupDetailList', JSON.stringify(groupDetailObj), (err, reply) => {
+  //       if (err) throw new Error(err)
+  //       console.log('number Of rooms = ', reply)
+  //       cb(reply)
+  //     })
+  //   })
+  // }
+  static saveGroupDetail (obj, cb) {
     Rooms.saveUserDetailList(obj)
-    redisClient.hgetall('groupDetailObj', (err, groupDetailObj) => {
-      if (err) throw new Error(err)
-      redisClient.rpush('groupDetailList', JSON.stringify(groupDetailObj), (err, reply) => {
-        if (err) throw new Error(err)
-        console.log('number Of rooms = ', reply)
-        cb(reply)
-      })
+    redisClient.hmset('groupAdminSet', obj.groupName, obj.admin)
+    obj.users.forEach(user => {
+      redisClient.rpush(obj.groupName.concat(_userExtn), user)
     })
+    cb()
   }
-  static getGroupDetailList (groupIndex, cb) {
-    redisClient.lindex('groupDetailList', groupIndex, (err, obj) => {
-      if (err) throw new Error(err)
-      console.log('obj from lindex --> ', obj)
-      console.log('parsed obj-> ', JSON.parse(obj))
-      cb(JSON.parse(obj))
-    })
-  }
+
+  // static getGroupDetailObj (groupIndex, cb) {
+  //   redisClient.lindex('groupDetailList', groupIndex, (err, obj) => {
+  //     if (err) throw new Error(err)
+  //     console.log('obj from lindex --> ', obj)
+  //     console.log('parsed obj-> ', JSON.parse(obj))
+  //     cb(JSON.parse(obj))
+  //   })
+  // }
+  // static getGroupDetailList (cb) {
+  //   redisClient.lrange('groupDetailList', 0, -1, (err, list) => {
+  //     if(err) throw new Error(err)
+  //     cb(list)
+  //   })
+  // }
   static saveUserDetailList (obj) {
     console.log('<roomdb.js saveUserDetailList> obj -> ', obj)
     let groupName = obj.groupName
@@ -39,11 +56,11 @@ class Rooms {
     let admin = obj.admin
     if (users.indexOf(admin) === -1) users.push(admin)
     for (let user of users) {
-      redisClient.rpush(user, groupName)
+      redisClient.rpush(user.concat(_groupExtn), groupName)
     }
   }
   static getUserDetailList (userName, cb) {
-    redisClient.lrange(userName, 0, -1, (err, list) => {
+    redisClient.lrange(userName.concat(_groupExtn), 0, -1, (err, list) => {
       console.log('<roomdb.js, getUserDetailList > userList ', list)
       if (err) throw new Error(err)
       cb(list)
@@ -56,7 +73,7 @@ class Rooms {
     })
     redisClient.hgetall('messageSenderObj', (err, messageSenderObj) => {
       if (err) throw new Error(err)
-      redisClient.rpush(msg.groupName, JSON.stringify(messageSenderObj), (err, reply) => {
+      redisClient.rpush(msg.groupName.concat(_messages), JSON.stringify(messageSenderObj), (err, reply) => {
         if (err) throw new Error(err)
         console.log('total message count in ', msg.groupName, ' ', reply)
         cb(reply)
@@ -64,7 +81,8 @@ class Rooms {
     })
   }
   static getGroupMessages (groupName, cb) {
-    redisClient.lrange(groupName, 0, -1, (err, messagesList) => {
+    console.log('<roomdb.js getGroupMessages > groupName -> ', groupName)
+    redisClient.lrange(groupName.concat(_messages), 0, -1, (err, messagesList) => {
       if (err) throw new Error(err)
       console.log('<roomdb.js getGroupMessages > messagesList = ', messagesList)
       console.log('<roomdb.js getGroupMessages > messages = ', messagesList)
